8d639d858dd2e070b832b38657c344ab5a5d419a
石山岭 <2329827114@qq.com>
2023-03-17 14:59:33 +0800

    小程序首次提交

---
+++ miniprogram/pages/components/SSLNavigationBar/ssl-navigation-bar.ts
@@ -0,0 +1,27 @@
+// components/SSLNavigationBar/ssl-navigation-bar.ts
+const app = getApp()
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    navBarHeight: app.globalData.navBarHeight,
+    menuRight: app.globalData.menuRight,
+    menuTop: app.globalData.menuTop,
+    menuHeight: app.globalData.menuHeight,
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
